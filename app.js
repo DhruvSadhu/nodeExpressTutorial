e3467a007e19ece71c9dcfc8d9a5ba2c4d7bcf82
dsadhu <dsadhu@ncsu.edu>
2023-04-14 16:03:12 -0400

    setting up middleware and layout

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+// helps pass data thru forms 
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+require('dotenv').config()
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Parsing middleware
+//Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Parse application/json
+app.use(bodyParser.json());
+
+//for static files
+//now any files located in the 'public' directory can be accessed using their respective URLS
+//relative to the root of the application
+//eg, main.css can be accessed at localhost:PORT/main.css
+app.use(express.static('public'));
+
+
+//Register a templating engine, for rednering of dynamic content, enables use of placeholders for data
+//default file extension is .handlebars, this shortens it
+app.engine('hbs', exphbs.engine({'extname':'.hbs'}));
+app.set('view engine', 'hbs');
+
+
+//router
+app.get('', (req, res) =>{
+    res.render('home');
+});
+
+app.listen(port, ()=> {
+    console.log(`listening on port ${port}`)
+})
